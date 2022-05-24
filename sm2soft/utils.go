93bffc9b3a8981528f847455cff05d94fa66ef0f
xiazeyin <405845829@163.com>
2022-05-24 14:36:38 +0800

    first commit

---
+++ sm2soft/utils.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2022 zhaochun
+// gmgo is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+/*
+sm2soft 是sm2的纯软实现，基于tjfoc国密算法库`tjfoc/gmsm`做了少量修改。
+对应版权声明: thrid_licenses/github.com/tjfoc/gmsm/版权声明
+*/
+
+package sm2soft
+
+import (
+	"encoding/asn1"
+	"math/big"
+)
+
+func Decompress(a []byte) *PublicKey {
+	var aa, xx, xx3 sm2P256FieldElement
+
+	P256Sm2()
+	x := new(big.Int).SetBytes(a[1:])
+	curve := sm2P256
+	sm2P256FromBig(&xx, x)
+	sm2P256Square(&xx3, &xx)       // x3 = x ^ 2
+	sm2P256Mul(&xx3, &xx3, &xx)    // x3 = x ^ 2 * x
+	sm2P256Mul(&aa, &curve.a, &xx) // a = a * x
+	sm2P256Add(&xx3, &xx3, &aa)
+	sm2P256Add(&xx3, &xx3, &curve.b)
+
+	y2 := sm2P256ToBig(&xx3)
+	y := new(big.Int).ModSqrt(y2, sm2P256.P)
+	if getLastBit(y) != uint(a[0]) {
+		y.Sub(sm2P256.P, y)
+	}
+	return &PublicKey{
+		Curve: P256Sm2(),
+		X:     x,
+		Y:     y,
+	}
+}
+
+func Compress(a *PublicKey) []byte {
+	buf := []byte{}
+	yp := getLastBit(a.Y)
+	buf = append(buf, a.X.Bytes()...)
+	if n := len(a.X.Bytes()); n < 32 {
+		buf = append(zeroByteSlice()[:(32-n)], buf...)
+	}
+	buf = append([]byte{byte(yp)}, buf...)
+	return buf
+}
+
+type sm2Signature struct {
+	R, S *big.Int
+}
+
+func SignDigitToSignData(r, s *big.Int) ([]byte, error) {
+	return asn1.Marshal(sm2Signature{r, s})
+}
+
+func SignDataToSignDigit(sign []byte) (*big.Int, *big.Int, error) {
+	var sm2Sign sm2Signature
+
+	_, err := asn1.Unmarshal(sign, &sm2Sign)
+	if err != nil {
+		return nil, nil, err
+	}
+	return sm2Sign.R, sm2Sign.S, nil
+}
---
+++ sm4/sm4_test.go
@@ -0,0 +1,117 @@
+// Copyright (c) 2022 zhaochun
+// gmgo is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+package sm4
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSm4(t *testing.T) {
+	key := []byte("1234567890abcdef")
+	data := []byte("天行健君子以自强不息")
+
+	fmt.Println("---------------- testCBC ----------------")
+	err := testCBC(key, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("---------------- testCFB ----------------")
+	err = testCFB(key, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("---------------- testOFB ----------------")
+	err = testOFB(key, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("---------------- testGCM ----------------")
+	err = testGCM(key, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testCBC(key, data []byte) error {
+	iv, encryptData, err := Sm4EncryptCbc(data, key)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("CBC iv 16进制 : %x\n", iv)
+	fmt.Printf("CBC encryptData 16进制 : %x\n", encryptData)
+
+	plainData, err := Sm4DecryptCbc(encryptData, key, iv)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("CBC plainData : %s\n", plainData)
+	return nil
+}
+
+func testCFB(key, data []byte) error {
+	iv, encryptData, err := Sm4EncryptCfb(data, key)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("CFB iv 16进制 : %x\n", iv)
+	fmt.Printf("CFB encryptData 16进制 : %x\n", encryptData)
+
+	plainData, err := Sm4DecryptCfb(encryptData, key, iv)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("CFB plainData : %s\n", plainData)
+	return nil
+}
+
+func testOFB(key, data []byte) error {
+	iv, encryptData, err := Sm4EncryptOfb(data, key)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("OFB iv 16进制 : %x\n", iv)
+	fmt.Printf("OFB encryptData 16进制 : %x\n", encryptData)
+
+	plainData, err := Sm4DecryptOfb(encryptData, key, iv)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("OFB plainData : %s\n", plainData)
+	return nil
+}
+
+func testGCM(key, data []byte) error {
+	nonce, encryptData, err := Sm4EncryptGcm(data, key)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("GCM nonce 16进制 : %x\n", nonce)
+	fmt.Printf("GCM encryptData 16进制 : %x\n", encryptData)
+
+	plainData, err := Sm4DecryptGcm(encryptData, key, nonce)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("GCM plainData : %s\n", plainData)
+	return nil
+}
+
+func BenchmarkSm4(t *testing.B) {
+	key := []byte("1234567890abcdef")
+	data := []byte("天行健君子以自强不息")
+	t.ReportAllocs()
+	for i := 0; i < t.N; i++ {
+		nonce, encryptData, _ := Sm4EncryptGcm(data, key)
+		Sm4DecryptGcm(encryptData, key, nonce)
+	}
+}
